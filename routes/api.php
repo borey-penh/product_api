03a42d37537f68bb5d8d31030055a77a3ded1897
BLACKBOXAI <blackboxai@example.com>
2026-06-20 14:37:22 +0700

    add and fix error

--- routes/api.php
+++ routes/api.php
@@ -2,35 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Product;
 use App\Http\Controllers\Api\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-Route::get('/products', function(){
-
-    return Product::all();
-
-});
-
-
-Route::post('/products', function (Request $request) {
-
-    $request->validate([
-        'name' => 'required',
-        'price' => 'required|numeric'
-    ]);
-
-    $product = Product::create([
-        'name' => $request->name,
-        'price' => $request->price
-    ]);
-
-    return response()->json($product);
-});
-
-
 Route::apiResource('products', ProductController::class);
+
